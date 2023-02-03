fccf825e675fda324aec56d1438cb7874f0d2eac
Juan Barragan <juan@barragan.fr>
2023-02-03 09:03:46 +0100

    initial

---
+++ library/src/tic_tac_toe/logic/models.py
@@ -0,0 +1,60 @@
+import enum
+import re
+from dataclasses import dataclass
+from functools import cached_property
+
+class Mark(str, enum.Enum):
+        CROSS = "X"
+        NAUGHT = "O"
+
+    
+        @property
+        def other(self) -> "Mark":
+            return Mark.CROSS if self is Mark.NAUGHT else Mark.NAUGHT
+
+@dataclass(frozen=True)
+class Grid:
+    cells: str = " "*9
+
+    def __post_init__(self)->None:
+        if not re.match(r"^[\sXO]{9}$", self.cells):
+            raise ValueError("Must contain 9 cell of: X, O or space")
+
+    @cached_property
+    def x_count(self)-> int:
+        return self.cells.count("X")
+
+    @cached_property
+    def o_count(self)->int:
+        return self.cells.count("O")
+
+    @cached_property
+    def empty_count(self)->int:
+        return self.cells.count(" ")
+
+@dataclass(frozen=True)
+class Move:
+    mark: Mark
+    cell_index: int
+    before_state: "GameState"
+    after_state: "GameState"
+
+@dataclass(frozen=True)
+class GameState:
+    grid: Grid
+    starting_mark: Mark = Mark("X")
+
+    # def __init__(self, grid: Grid, starting_mark: Mark = Mark("X")):
+    #     self.grid = grid
+    #     self.starting_mark = starting_mark
+
+    @cached_property
+    def current_mark(self)->Mark:
+        if self.grid.x_count == self.grid.o_count:
+            return self.starting_mark
+        else:
+            return self.starting_mark.other
+    
+    @cached_property
+    def game_not_started(self)->bool:
+        return self.grid.empty_count == 9
